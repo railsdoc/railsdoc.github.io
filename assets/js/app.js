45b36c6f1a57f4745186afb42c369a0461ba17fe
toshimaru <toshimaru@users.noreply.github.com>
2021-04-17 15:02:20 +0000

    deploy: 6026c66d9c05e43494a46422431cec2e6154d64d

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+$(() => {
+  // anchor-js
+  anchors.options.visible = 'always';
+  anchors.add();
+
+  // highlight.js
+  hljs.initHighlighting();
+
+  $(".sidebar-sticky .icon").on("click", function (e) {
+    $(this).siblings("ul").toggle();
+    this.classList.toggle("icon-opened");
+  });
+  $(`.sidebar-content a[href='${window.location.pathname}']`)
+    .attr("class", "active-link")
+    .parents('ul')
+    .show();
+});
