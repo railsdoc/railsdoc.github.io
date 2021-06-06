309ed59eaaaaea80664c7e69ba07c0b1292be0e1
Toshimaru <me@toshimaru.net>
2021-06-06 16:08:09 +0900

    Load sidemenu navigation via jQuery.load (#83)
    * load sidemenu navigation via jQuery.load
    
    * Set rootPath for JavaScript
    
    * Generate new navigation.html for JS
    
    * Generate single navigations for each Rails version
    
    Single navigation.html instead of _includes/navigation.html
    
    * load each navigations

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -6,12 +6,14 @@ $(() => {
   // highlight.js
   hljs.highlightAll();
 
-  $(".sidebar-sticky .icon").on("click", function (e) {
-    $(this).siblings("ul").toggle();
-    this.classList.toggle("icon-opened");
-  });
-  $(`.sidebar-content a[href='${window.location.pathname}']`)
-    .attr("class", "active-link")
-    .parents('ul')
-    .show();
+  $("#navigation").load(`${config.rootPath}navigation.html`, function() {
+    $(".sidebar-sticky .icon").on("click", function (e) {
+      $(this).siblings("ul").toggle();
+      this.classList.toggle("icon-opened");
+    });
+    $(`.sidebar-content a[href='${window.location.pathname}']`)
+      .attr("class", "active-link")
+      .parents('ul')
+      .show();
+  })
 });
