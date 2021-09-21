8af573d7d639f96a9444b384a6936bae01d448a7
toshimaru <me@toshimaru.net>
2021-09-21 09:16:10 +0900

    Add navigation toggle animation
    FIXME: should be replaced with Vanilla JS and CSS

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -6,11 +6,15 @@ $(() => {
   // highlight.js
   hljs.highlightAll();
 
-  $("#navigation").load(`${config.rootPath}navigation.html`, function() {
-    $(".sidebar-sticky .icon").on("click", function (e) {
-      $(this).siblings("ul").toggle();
+  $("#navigation").load(`${config.rootPath}navigation.html`, () => {
+    // Toggle child lists
+    $(".sidebar-sticky .icon").on("click", function () {
+      // FIXME: use vanilla JS and CSS instead of jQuery
+      $(this).siblings("ul").toggle(500);
       this.classList.toggle("icon-opened");
     });
+
+    // Activate current page
     $(`.sidebar-content a[href='${window.location.pathname}']`)
       .attr("class", "active-link")
       .parents('ul')
