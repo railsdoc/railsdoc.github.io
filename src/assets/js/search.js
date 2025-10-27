24c60ba2127f10bc12ea4514a53f2795dedda6f1
Toshimaru <me@toshimaru.net>
2025-10-27 09:18:27 +0900

    chore: Add window for addEventListener

--- src/assets/js/search.js
+++ src/assets/js/search.js
@@ -1,24 +1,24 @@
 $(() => {
-  const searchForm = document.getElementById("search-form")
-  const searchValue = (new URLSearchParams(location.search)).get("q")
+  const searchForm = document.getElementById("search-form");
+  const searchValue = new URLSearchParams(location.search).get("q");
 
   if (searchValue) {
-    searchForm.value = searchValue
+    searchForm.value = searchValue;
   }
 
-  addEventListener("keydown", (e) => {
+  window.addEventListener("keydown", (e) => {
     switch (e.key) {
-      case '/':
-        if (document.activeElement === searchForm) break
+      case "/":
+        if (document.activeElement === searchForm) break;
 
-        e.preventDefault()
-        searchForm.focus()
-        break
-      case 'Escape':
+        e.preventDefault();
+        searchForm.focus();
+        break;
+      case "Escape":
         if (document.activeElement === searchForm) {
-          searchForm.blur()
+          searchForm.blur();
         }
-        break
+        break;
     }
-  })
+  });
 });
